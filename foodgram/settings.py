6af8db4ee988728d501c1263aa82b82908813aaf
Maslov-Moscow <trubraver@gmail.com>
2020-12-10 01:25:03 +0300

    compose fix

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -91,12 +91,7 @@ DATABASES = {
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'postgres',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'postgres',
-        # 'HOST': 'db',
-        # 'PORT': '5432',
+
     }
 } 
 # Password validation
